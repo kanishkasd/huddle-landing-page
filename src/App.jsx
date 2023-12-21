7b2bee3a5663e165754549751ae371b847c531c0
kanishkasd <kanishkasandaru2048@gmail.com>
2023-12-21 21:18:04 +0530

    create desktop version

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
 import './App.css'
-import Header from './components/Header'
+import backgroundImage from './assets/bg-desktop.svg'
+import logo from './assets/logo.svg'
+import mockup from './assets/illustration-mockups.svg'
 
 function App() {
-    return(
+    const styles = {
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: 'contain',
+        backgroundRepeat: 'no-repeat',
+        height: '100vh',
+    };
+    return (
+        <body className="bg-violet font-poppins" style={styles}>
+            <header className="w-36 pt-10 ml-20">
+                <img src={logo} alt="logo" />
+            </header>
+            <div className="flex">
+            <div className="left-side w-2/5 mt-20 ml-20" >
+                <img src={mockup} alt="mockup" />
+            </div>
 
-        <Header /> 
+            <div className="right-side w-5/12 ml-20 mt-32 text-white">
+        
+                <h3 className="w-96 leading-snug text-3xl font-semibold">Build The Community Your Fans Will Love</h3>
+                <p className="mt-4 w-[500px] text-white text-opacity-70">
+                    Huddle re-imagines the way we build communities. You have a voice, but so does your audience.
+                   Create connections with your users as you engage in genuine discussion.</p>
+                <button className="w-36 h-10 bg-white text-violet text-sm rounded-3xl mt-4 shadow hover:bg-soft-magenta hover:text-white">Register</button>
+            </div>
+        </div>
+        </body>
     )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-      "./index.html",
-      "./src/**/*.{vue,js,ts,jsx,tsx}"
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./index.html",
+        "./src/**/*.{vue,js,ts,jsx,tsx}"
+    ],
+    theme: {
+    extend: 
+        {
+            colors: {
+                'violet': 'hsl(257, 40%, 49%)',
+                'soft-magenta': 'hsl(300, 69%, 71%)'
+
+            },
+            fontFamily: {
+               'poppins': [ 'Poppins', 'sans-serif' ]
+            },
+
+            boxShadow: {
+              DEFAULT: '1px 1px 2px black'}
+        },
+    },
+    plugins: [],
 }
 
